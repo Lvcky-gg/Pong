4b53cf63e955fa73600e891abf68f97fe9be6739
lvcky-gg <matadorcrab@gmail.com>
2023-09-14 21:43:46 -0500

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+    pass
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+if __name__'__main':
+    PongApp().run()
